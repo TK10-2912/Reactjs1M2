48fbc9263b952fd448fe929c9866bfddf53600be
DepZaiTheNam <namthe.8888@gmail.com>
2024-03-28 22:59:16 +0700

    thoong tin thanh toan chua xong

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -6,6 +6,7 @@ import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 import { resetCart } from "../../redux/orebiSlice";
 import { emptyCart } from "../../assets/images/index";
 import ItemCard from "./ItemCard";
+import { Col, Row } from "antd";
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -33,74 +34,80 @@ const Cart = () => {
     <div className="max-w-container mx-auto px-4">
       <Breadcrumbs title="Cart" />
       {products.length > 0 ? (
-        <div className="pb-20">
-          <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
-            <h2 className="col-span-2">Product</h2>
-            <h2>Price</h2>
-            <h2>Quantity</h2>
-            <h2>Sub Total</h2>
-          </div>
-          <div className="mt-5">
-            {products.map((item) => (
-              <div key={item._id}>
-                <ItemCard item={item} />
-              </div>
-            ))}
-          </div>
-
-          <button
-            onClick={() => dispatch(resetCart())}
-            className="py-2 px-10 bg-red-500 text-white font-semibold uppercase mb-4 hover:bg-red-700 duration-300"
-          >
-            Reset cart
-          </button>
-
-          <div className="flex flex-col mdl:flex-row justify-between border py-4 px-4 items-center gap-2 mdl:gap-0">
-            <div className="flex items-center gap-4">
-              <input
-                className="w-44 mdl:w-52 h-8 px-4 border text-primeColor text-sm outline-none border-gray-400"
-                type="text"
-                placeholder="Coupon Number"
-              />
-              <p className="text-sm mdl:text-base font-semibold">
-                Apply Coupon
-              </p>
+        <Row>
+          <Col span={16}>
+            <div className="w-full h-20 bg-[#F5F7F7] text-primeColor hidden lgl:grid grid-cols-5 place-content-center px-6 text-lg font-titleFont font-semibold">
+              <h2 className="col-span-2">Product</h2>
+              <h2>Price</h2>
+              <h2>Quantity</h2>
+              <h2>Sub Total</h2>
             </div>
-            <p className="text-lg font-semibold">Update Cart</p>
-          </div>
-          <div className="max-w-7xl gap-4 flex justify-end mt-4">
-            <div className="w-96 flex flex-col gap-4">
-              <h1 className="text-2xl font-semibold text-right">Cart totals</h1>
-              <div>
-                <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
-                  Subtotal
-                  <span className="font-semibold tracking-wide font-titleFont">
-                    ${totalAmt}
-                  </span>
-                </p>
-                <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
-                  Shipping Charge
-                  <span className="font-semibold tracking-wide font-titleFont">
-                    ${shippingCharge}
-                  </span>
-                </p>
-                <p className="flex items-center justify-between border-[1px] border-gray-400 py-1.5 text-lg px-4 font-medium">
-                  Total
-                  <span className="font-bold tracking-wide text-lg font-titleFont">
-                    ${totalAmt + shippingCharge}
-                  </span>
+            <div className="mt-5">
+              {products.map((item) => (
+                <div key={item._id}>
+                  <ItemCard item={item} />
+                </div>
+              ))}
+            </div>
+
+            <button
+              onClick={() => dispatch(resetCart())}
+              className="py-2 px-10 bg-red-500 text-white font-semibold uppercase mb-4 hover:bg-red-700 duration-300"
+            >
+              Reset cart
+            </button>
+
+            <div className="flex flex-col mdl:flex-row justify-between border py-4 px-4 items-center gap-2 mdl:gap-0">
+              <div className="flex items-center gap-4">
+                <input
+                  className="w-44 mdl:w-52 h-8 px-4 border text-primeColor text-sm outline-none border-gray-400"
+                  type="text"
+                  placeholder="Coupon Number"
+                />
+                <p className="text-sm mdl:text-base font-semibold">
+                  Apply Coupon
                 </p>
               </div>
-              <div className="flex justify-end">
-                <Link to="/paymentgateway">
-                  <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
-                    Proceed to Checkout
-                  </button>
-                </Link>
+              <p className="text-lg font-semibold">Update Cart</p>
+            </div>
+
+          </Col>
+          <Col span={8}>        
+            <div  class="block max-w-7xl gap-4 justify-end mt-4 p-6 bg-white border mx-5 rounded-lg ">           
+              <div className="w-96 flex flex-col gap-4">
+                <h1 className="text-2xl font-semibold text-right">Cart totals</h1>
+                <div>
+                  <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
+                    Subtotal
+                    <span className="font-semibold tracking-wide font-titleFont">
+                      ${totalAmt}
+                    </span>
+                  </p>
+                  <p className="flex items-center justify-between border-[1px] border-gray-400 border-b-0 py-1.5 text-lg px-4 font-medium">
+                    Shipping Charge
+                    <span className="font-semibold tracking-wide font-titleFont">
+                      ${shippingCharge}
+                    </span>
+                  </p>
+                  <p className="flex items-center justify-between border-[1px] border-gray-400 py-1.5 text-lg px-4 font-medium">
+                    Total
+                    <span className="font-bold tracking-wide text-lg font-titleFont">
+                      ${totalAmt + shippingCharge}
+                    </span>
+                  </p>
+                </div>
+                <div className="flex justify-end">
+                  <Link to="/paymentgateway">
+                    <button className="w-52 h-10 bg-primeColor text-white hover:bg-black duration-300">
+                      Proceed to Checkout
+                    </button>
+                  </Link>
+                </div>
               </div>
             </div>
-          </div>
-        </div>
+          </Col>
+        </Row>
+
       ) : (
         <motion.div
           initial={{ y: 30, opacity: 0 }}
