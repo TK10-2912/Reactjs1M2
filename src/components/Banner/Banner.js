8ef23d478aeb7ad7febb8e315c8feec569cc8f4b
LAPTOP-712TU6OO\ASUS <ttkhk20@gmail.com>
2024-04-03 14:54:29 +0700

    đẩy banner

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -86,19 +86,19 @@ const Banner = () => {
         style={
           i === dotActive
             ? {
-                width: "30px",
-                color: "#262626",
-                borderRight: "3px #262626 solid",
-                padding: "8px 0",
-                cursor: "pointer",
-              }
+              width: "30px",
+              color: "#262626",
+              borderRight: "3px #262626 solid",
+              padding: "8px 0",
+              cursor: "pointer",
+            }
             : {
-                width: "30px",
-                color: "transparent",
-                borderRight: "3px white solid",
-                padding: "8px 0",
-                cursor: "pointer",
-              }
+              width: "30px",
+              color: "transparent",
+              borderRight: "3px white solid",
+              padding: "8px 0",
+              cursor: "pointer",
+            }
         }
       >
         0{i + 1}
@@ -126,19 +126,19 @@ const Banner = () => {
               style={
                 i === dotActive
                   ? {
-                      width: "25px",
-                      color: "#262626",
-                      borderRight: "3px #262626 solid",
-                      cursor: "pointer",
-                      fontSize: "12px",
-                    }
+                    width: "25px",
+                    color: "#262626",
+                    borderRight: "3px #262626 solid",
+                    cursor: "pointer",
+                    fontSize: "12px",
+                  }
                   : {
-                      width: "25px",
-                      color: "transparent",
-                      borderRight: "3px white solid",
-                      cursor: "pointer",
-                      fontSize: "12px",
-                    }
+                    width: "25px",
+                    color: "transparent",
+                    borderRight: "3px white solid",
+                    cursor: "pointer",
+                    fontSize: "12px",
+                  }
               }
             >
               0{i + 1}
@@ -178,14 +178,48 @@ const Banner = () => {
     // Add more slides as needed
   ];
   return (
-    <div className="w-full bg-white">
-      <Slider {...settings}>
-        {slides.map((slide, index) => (
-          <CustomSlide key={index} {...slide} />
-        ))}
-      </Slider>
-    </div>
-  );
+    <div id="indicators-carousel" class="relative w-full" data-carousel="static">
+      <div class="relative h-56 overflow-hidden rounded-lg md:h-96">
+        <div class="hidden duration-700 ease-in-out" data-carousel-item="active">
+          <img src="/docs/images/carousel/carousel-1.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/docs/images/carousel/carousel-2.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/docs/images/carousel/carousel-3.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/docs/images/carousel/carousel-4.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+          <img src="/docs/images/carousel/carousel-5.svg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="..." />
+        </div>
+      </div>
+      <div class="absolute z-30 flex -translate-x-1/2 space-x-3 rtl:space-x-reverse bottom-5 left-1/2">
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
+        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
+      </div>
+      <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4" />
+          </svg>
+          <span class="sr-only">Previous</span>
+        </span>
+      </button>
+      <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+          <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4" />
+          </svg>
+          <span class="sr-only">Next</span>
+        </span>
+      </button>
+    </div>);
 };
 
 export default Banner;
--- src/components/home/Sale/Sale.js
+++ src/components/home/Sale/Sale.js
@@ -1,56 +1,82 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import {
-  saleImgOne,
-  saleImgTwo,
-  saleImgThree,
+	saleImgOne,
+	saleImgTwo,
+	saleImgThree,
 } from "../../../assets/images/index";
 import Image from "../../designLayouts/Image";
 import ShopNow from "../../designLayouts/buttons/ShopNow";
+import Banner from "../../Banner/Banner";
+import { Carousel, Col, Row } from "antd";
 
 const Sale = () => {
-  return (
-    <div className="py-20 flex flex-col md:flex-row items-center justify-between gap-4 lg:gap-10">
-      <div className="bg-[#f3f3f3] w-full md:w-2/3 lg:w-1/2 h-full flex flex-col justify-center items-center text-black">
-        <div className="aspect-w-4 aspect-h-3 w-full mb-4">
-          <Image className="h-full w-full object-cover" imgSrc={saleImgOne} />
-        </div>
-        <div className="text-left h-140 md:h-200 lg:h-260 w-full mx-4 ">
-          <div className="mx-8">
-            <h2 className="text-4xl md:text-5xl lg:text-5xl font-bold mb-6">
-              Imprimante sales
-            </h2>
-            <p className="text-lg md:text-xl lg:text-2xl mb-6">
-              Up to{" "}
-              <span className="text-4xl md:text-5xl lg:text-5xl font-bold">
-                30%
-              </span>{" "}
-              sales for all impriamnte{" "}
-            </p>
-            <div className=" mb-8">
-              <ShopNow />
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="w-full md:w-2/3 lg:w-1/2 h-auto flex flex-col gap-4 lg:gap-10">
-        <div className="h-1/2 w-full">
-          <Link to="/shop">
-            <Image className="h-full w-full object-cover" imgSrc={saleImgTwo} />
-          </Link>
-        </div>
-        <div className="h-1/2 w-full">
-          <Link to="/shop">
-            <Image
-              className="h-full w-full object-cover"
-              imgSrc={saleImgThree}
-            />
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+	const contentStyle = {
+		height: '444px',
+		color: '#fff',
+		// lineHeight: '160px',
+		textAlign: 'center',
+		border: "none",
+		borderRadius: "50%",
+	};
+	return (
+		<Row style={{ marginTop: "10px", marginBottom:"10px" }} gutter={[8, 8]}>
+			<Col span={16}>
+				<Carousel style={{ height: "100%" }} autoplay >
+					<div>
+						<h3 style={contentStyle}> <Link to="/shop">
+							<Image
+								className="h-full w-full "
+								imgSrc={saleImgThree}
+							/>
+						</Link></h3>
+					</div>
+					<div>
+						<h3 style={contentStyle}> <Link to="/shop">
+							<Image
+								className="h-full w-full "
+								imgSrc={saleImgThree}
+							/>
+						</Link></h3>
+					</div>
+					<div>
+						<h3 style={contentStyle}> <Link to="/shop">
+							<Image
+								className="h-full w-full "
+								imgSrc={saleImgThree}
+							/>
+						</Link></h3>
+					</div>
+					<div>
+						<h3 style={contentStyle}> <Link to="/shop">
+							<Image
+								className="h-full w-full "
+								imgSrc={saleImgThree}
+							/>
+						</Link></h3>
+					</div>
+				</Carousel>
+			</Col>
+			<Col span={8}>
+				<Col span={24} style={{ marginBottom: "10px" }}>
+					<Link to="/shop">
+						<Image
+							className="rounded"
+							imgSrc={saleImgThree}
+						/>
+					</Link>
+				</Col>
+				<Col span={24}>
+					<Link to="/shop">
+						<Image
+							className="rounded-full p-4"
+							imgSrc={saleImgThree}
+						/>
+					</Link>
+				</Col>
+			</Col>
+		</Row>
+	);
 };
 
 export default Sale;
